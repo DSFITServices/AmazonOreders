5dad063844238c91cd4f19d07aacbe10d65c6f4c
David Francis <dandmitservices@gmail.com>
2017-11-20 11:48:43 +0000

    Added files

---
+++ amazonOrders/AmazonOrderProcessing.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Net.Http;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using MWSClientCsRuntime;
+using MarketplaceWebService;
+using MarketplaceWebService.Attributes;
+using MarketplaceWebService.Model;
+
+
+namespace amazonOrders
+{
+    class AmazonOrderProcessing
+    {
+        private string mwsAccessKey;
+        private string mwsSecretAccessKey;
+        private string mwsServiceUrl;
+        private string mwsAppName;
+        private string mwsAppVersion;
+        private string mwsCode;
+        private string mwsMerchant;
+        private MarketplaceWebServiceConfig mwsConfig;
+        private ReportType reportType;
+        private List<object> amzOrders;
+        private FileInfo fileInfo;
+        
+
+
+        public string AccessKey { get => mwsAccessKey; private set => mwsAccessKey = value; }
+        public string SecretAccessKey { get => mwsSecretAccessKey; private set => mwsSecretAccessKey = value; }
+        public string ServiceUrl { get => mwsServiceUrl; private set => mwsServiceUrl = value; }
+        public string AppName { get => mwsAppName; set => mwsAppName = value; }
+        public string AppVersion { get => mwsAppVersion; private set => mwsAppVersion = value; }
+        public string Code { get => mwsCode; private set => mwsCode = value; }
+        public string MwsMerchant { get => mwsMerchant; set => mwsMerchant = value; }
+        internal ReportType ReportType1 { get => reportType; set => reportType = value; }
+        public List<object> AmzOrders { get => amzOrders; set => amzOrders = value; }
+
+        public enum ReportType
+        {
+            _GET_FLAT_FILE_ORDERS_DATA_,
+            _GET_MERCHANT_LISTINGS_DATA_,
+            //_GET_ORDERS_DATA_, // Order 100 not allowed at this time
+            _GET_XML_ALL_ORDERS_DATA_BY_LAST_UPDATE_,
+        };
+
+
+        public AmazonOrderProcessing(string accessKey, string secretAccessKey, string serviceUrl, string appName, string appVersion, string code, string merchant)
+        {
+            mwsAccessKey = accessKey;
+            mwsSecretAccessKey = secretAccessKey;
+            mwsServiceUrl = serviceUrl;
+            mwsAppName = appName;
+            mwsAppVersion = appVersion;
+            mwsCode = code;
+            mwsMerchant = merchant;
+            
+
+            //Thread.Sleep(15000);
+        }
+
+        private MarketplaceWebServiceClient GetClient()
+        {
+            mwsConfig = new MarketplaceWebServiceConfig();
+            mwsConfig.ServiceURL = this.ServiceUrl;
+            mwsConfig.SetUserAgentHeader(this.mwsAppName, this.mwsAppVersion, this.mwsCode, new string[] { });
+
+            return new MarketplaceWebServiceClient(AccessKey, SecretAccessKey,AppName,AppVersion,mwsConfig);
+        }
+
+
+
+        public RequestReportResult GetRequestOrderResult(DateTime startDate, DateTime endDate, ReportType reportType)
+        {
+            
+            RequestReportRequest reportRequestRequest = new RequestReportRequest();
+            reportRequestRequest.Merchant = mwsMerchant;
+            //reportRequestRequest.
+            reportRequestRequest.ReportType = reportType.ToString();
+
+            // If this is an order report, get only last week
+            if (reportType == ReportType._GET_FLAT_FILE_ORDERS_DATA_)
+            {
+                reportRequestRequest.StartDate = startDate;
+                reportRequestRequest.EndDate = endDate;
+            }
+
+
+            return GetClient().RequestReport(reportRequestRequest).RequestReportResult;
+        }
+
+
+        public List<ReportRequestInfo> GetReportRequests()
+        {
+                GetReportRequestListRequest reportlistReqests = new GetReportRequestListRequest();
+                reportlistReqests.Merchant = mwsMerchant;
+                GetReportRequestListResponse reportlistResponse = GetClient().GetReportRequestList(reportlistReqests);
+                return reportlistResponse.GetReportRequestListResult.ReportRequestInfo;
+        }
+
+        public FileInfo GetReportFile(ReportRequestInfo reportInfo)
+        {
+            GetReportRequest reportRequest = new GetReportRequest();
+            reportRequest.Merchant = mwsMerchant;
+            reportRequest.WithReportId(reportInfo.GeneratedReportId);
+
+            GetReportResponse reportResponse = new GetReportResponse();
+
+            string path = "Z:/Repos/amazonOrders/amazonOrders/ReportDownloaded/Report-" + reportInfo.ReportType + "-" + reportInfo.GeneratedReportId + ".txt";
+
+            using (FileStream stream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                reportRequest.Report = stream;
+                reportResponse = GetClient().GetReport(reportRequest);
+            }
+
+            return new FileInfo(path);
+        }
+
+
+        
+        
+
+    }
+}
---
+++ amazonOrders/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+//using MWSClientCsRuntime;
+using MarketplaceWebService;
+using MarketplaceWebService.Model;
+using amazonOrders;
+
+namespace amazonOrders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AmazonOrderProcessing Amz = new AmazonOrderProcessing("AKIAIMSOQ3ULEO44P2GA", "Eu09VI5yylj6BLWwnMxkJhQjUoVjfMr+hKTT+nzE", "https://mws.amazonservices.co.uk", "MyApp", "0.01a", "C#", "A10HS0L7N8OCDE");
+            
+            Console.WriteLine(Amz.AppName);
+
+            DateTime startDate = new DateTime(2017, 10, 10, 00, 00, 00);
+            DateTime endDate = DateTime.Now;
+
+           Amz.GetRequestOrderResult(startDate, endDate, AmazonOrderProcessing.ReportType._GET_FLAT_FILE_ORDERS_DATA_);
+
+            Console.WriteLine("Amazon Order Request sent");
+
+            
+
+            List<ReportRequestInfo> ReportInfo = new List<ReportRequestInfo>();
+            do
+            {
+                //Thread.Sleep(1 * 60000);
+                ReportInfo = Amz.GetReportRequests();
+                if (ReportInfo[0].ReportProcessingStatus != "_DONE_")
+                {
+                    Console.WriteLine("Waiting For Report to be Processed.");
+                    Thread.Sleep(2 * 60000);
+                }
+            } while (ReportInfo[0].ReportProcessingStatus != "_DONE_");
+
+
+
+            FileInfo fileInfo = Amz.GetReportFile(ReportInfo[0]);
+
+            Console.WriteLine("Safe to Exit");
+            Console.ReadLine();
+        }
+
+        
+    }
+}
